3979072135e25bce83958a5c799ca8cf769af7f6
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2025-11-26 12:11:55 +0100

    test: Add/update validation test

--- src/tests/utils/valitation.test.js
+++ src/tests/utils/valitation.test.js
@@ -1,13 +1,23 @@
-import { expect, it } from 'vitest';
+import { expect, it, describe } from 'vitest';
 import { validateUserName } from '../../utils/validation';
 
-it('checks if username is at least 3 characters', () => {
-  // Arrange: Set up the test data
-  const username = 'Anne';
-
-  // Act: Call the function being tested
-  const result = validateUserName(username);
-
-  // Assert: Check if the result matches the expected output
-  expect(result).toBe(true);
+describe('validateUserName', () => {
+  it('returns true for username with 3 characters', () => {
+    // Arrange: Set up the test data
+    const username = 'Anne';
+    // Act: Call the function being tested
+    const result = validateUserName(username);
+    // Assert: Check if the result matches the expected output
+    expect(result).toBe(true);
+  });
+  it('returns true if username has more than 3 characters', () => {
+    const username = 'Anne-Zwift';
+    const result = validateUserName(username);
+    expect(result).toBe(true);
+  });
+  it('returns false for username with less than 3 characters', () => {
+    const username = 'Zo';
+    const result = validateUserName(username);
+    expect(result).toBe(false);
+  });
 });
